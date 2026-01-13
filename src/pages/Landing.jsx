052019d318a768b5973cb63ba0f3e4df0e4356af
Kshitij Jain <2024btechaimlkshitij18489@poornima.edu.in>
2026-01-13 21:21:54 +0530

    Update the content of Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -239,6 +239,14 @@ const Landing = () => {
                   </div>
                 ))}
               </div>
+              <div className="mt-6 text-center">
+                <button className="text-blue-600 hover:text-blue-800 font-medium text-sm flex items-center justify-center w-full sm:w-auto mx-auto">
+                  View More Brokers
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                  </svg>
+                </button>
+              </div>
             </div>
 
             {/* Divider */}
@@ -262,6 +270,14 @@ const Landing = () => {
                     <p className="text-sm text-gray-500 mt-1">Check Eligibility & Apply</p>
                   </div>
                 ))}
+              </div>
+              <div className="mt-6 text-center">
+                <button className="text-blue-600 hover:text-blue-800 font-medium text-sm flex items-center justify-center w-full sm:w-auto mx-auto">
+                  View More Credit Cards
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                  </svg>
+                </button>
               </div>
             </div>
           </div>
@@ -293,16 +309,6 @@ const Landing = () => {
                     name: 'LinkedIn', 
                     icon: 'M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z',
                     url: 'https://www.linkedin.com/in/kshitij-kj-jain-422025342/'
-                  },
-                  { 
-                    name: 'Twitter', 
-                    icon: 'M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z',
-                    url: '#'
-                  },
-                  { 
-                    name: 'Facebook', 
-                    icon: 'M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z',
-                    url: '#'
                   }
                 ].map((social) => (
                   <a
