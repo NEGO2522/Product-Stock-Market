6219541b8eb1b0dc66634f0325c73ae2db5046d2
Kshitij Jain <2024btechaimlkshitij18489@poornima.edu.in>
2026-01-20 09:44:19 +0530

    Update the image

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -159,7 +159,7 @@ const Landing = () => {
           <div className="lg:w-1/2 mt-6 lg:mt-0">
             <div className="bg-white p-3 rounded-xl shadow-xl h-72 md:h-80">
               <img 
-                src="/Stock.jpg" 
+                src="/stock.jpg" 
                 alt="Stock Market Data Visualization"
                 className="w-full h-full object-cover rounded-lg"
               />
